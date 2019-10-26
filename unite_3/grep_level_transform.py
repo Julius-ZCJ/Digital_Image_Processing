b04fdb4ffb95274778ded895e8b6251f73c22534
Julius-ZCJ <1484601833@qq.com>
2019-10-26 11:48:42 +0800

    1. add ignore

--- unite_3/grep_level_transform.py
+++ unite_3/grep_level_transform.py
@@ -70,8 +70,8 @@ def power_transform(img, constant_c, constant_r):
     幂律变换又称为伽马变换（gamma）
     1.调节r值改变函数形状，调节c值为了将像素映射到0-255
     2.
-    当γ>1时，会拉伸图像中灰度级较高的区域，压缩灰度级较低的部分。
-    当γ<1时，会拉伸图像中灰度级较低的区域，压缩灰度级较高的部分。
+    当γ>1时，会拉伸图像中灰度级较高的区域，压缩灰度级较低的部分。(过曝)
+    当γ<1时，会拉伸图像中灰度级较低的区域，压缩灰度级较高的部分。（过暗）
     当γ=1时，该灰度变换是线性的，此时通过线性方式改变原图像。
     总体而言就是增强对比度。
     处理图像对比度偏低的有良好效果
@@ -92,13 +92,74 @@ def power_transform(img, constant_c, constant_r):
         table[i] = constant_c*i**constant_r
     new_img = np.uint8(cv2.LUT(img, table)+0.5)
     return new_img
+
+def contrastStretch(img, coord_1, coord_2):
+    x1 = coord_1[0]
+    x2 = coord_2[0]
+    k1 = coord_1[1]/coord_1[0]
+    k2 = (coord_2[1]-coord_1[1])/(coord_2[0]-coord_1[0])
+    b2 = coord_1[1] - k2*coord_1[0]
+    k3 = (255 - coord_2[1])/(255 - coord_2[0])
+    b3 = coord_2[1] - k3*coord_2[0]
+    def show_plot():
+        xx = np.arange(0, 255, 0.01)
+        yy = []
+        y = 0
+        for x in xx:
+            if x <= x1:
+                y = k1*x
+            elif x > x1 and x < x2:
+                y = k2*x + b2
+            else:
+                y = k3*x + b3
+            yy.append(y)
+        plt.plot(xx, yy, 'r', linewidth=1)
+        plt.rcParams['font.sans-serif']=['SimHei'] #正常显示中文标签
+        plt.title(u'分段函数函数')
+        plt.xlim([0, 255]), plt.ylim([0, 255])
+        plt.show()
+    
+    show_plot()
+    h, w = img.shape[:2]
+    new_img = np.zeros(img.shape, np.uint8)
+    for i in range(h):
+        for j in range(w):
+            if img[i][j] <= x1:
+                new_img[i][j] = np.uint8(k1*img[i][j])
+            elif img[i][j] > x1 and img[i][j] < x2:
+                new_img[i][j] = np.uint8(k2*img[i][j] + b2)
+            else:
+                new_img[i][j] = np.uint8(k3*img[i][j] + b3)
+    
+    return new_img
+    
+    
+def main():
+    
+    
+    path = r'../ImageData\ch02\bubbles.tif'
+    img =cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    unknow_transform(img)
+    
+    path = r'../ImageData\ch03\spectrum.tif'
+    img =cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    logarithm_transform(img, 45)
+    
+    path = r'../ImageData\ch02\bubbles.tif'
+    img =cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    power_transform(img, 0.000015, 3.0)
+    
     
     
 
 if __name__=='__main__':
-    path = r'../ImageData\ch02\bubbles.tif'
+    path = r'../ImageData\ch03\breast.tif'
+    path = 'logarithm_thansform.jpg'
     img =cv2.imread(path)
     print(img.shape)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    new_img = power_transform(img, 0.000015, 3.0)
-    cv2.imwrite('power_transform.jpg', new_img)
+    new_img = contrastStretch(img, (96, 32), (160, 224))
+    cv2.imwrite('contrastStretch.jpg', new_img)
